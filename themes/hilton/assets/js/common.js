a33dc76ac3918fa296970aac6ea2c749158d9069
albert <alb.carreras@gmail.com>
2023-11-12 15:40:51 +0100

    dark mode

--- themes/hilton/assets/js/common.js
+++ themes/hilton/assets/js/common.js
@@ -1,9 +1,50 @@
-document.addEventListener("DOMContentLoaded", function() {
-  'use strict';
+document.addEventListener("DOMContentLoaded", function () {
+  "use strict";
 
-  var toggleTheme = document.querySelector(".toggle-theme-js"),
+  var html = document.querySelector("html"),
+    menuOpenIcon = document.querySelector(".icon__menu"),
+    menuCloseIcon = document.querySelector(".nav__icon-close"),
+    menuList = document.querySelector(".main-nav"),
+    toggleTheme = document.querySelector(".toggle-theme-js"),
     btnScrollToTop = document.querySelector(".top");
 
+  /* =======================================================
+  // Menu + Theme Switcher
+  ======================================================= */
+  menuOpenIcon.addEventListener("click", () => {
+    menuOpen();
+  });
+
+  menuCloseIcon.addEventListener("click", () => {
+    menuClose();
+  });
+
+  function menuOpen() {
+    menuList.classList.add("is-open");
+  }
+
+  function menuClose() {
+    menuList.classList.remove("is-open");
+  }
+
+  if (toggleTheme) {
+    toggleTheme.addEventListener("click", () => {
+      darkMode();
+    });
+  }
+
+  // Theme Switcher
+  function darkMode() {
+    if (html.classList.contains("dark-mode")) {
+      html.classList.remove("dark-mode");
+      localStorage.removeItem("theme");
+      document.documentElement.removeAttribute("dark");
+    } else {
+      html.classList.add("dark-mode");
+      localStorage.setItem("theme", "dark");
+      document.documentElement.setAttribute("dark", "");
+    }
+  }
 
   /* ================================================================
   // Stop Animations During Window Resizing and Switching Theme Modes
@@ -28,39 +69,46 @@ document.addEventListener("DOMContentLoaded", function() {
     }
   }
 
-
   /* =======================
   // Responsive Videos
   ======================= */
-  reframe(".post__content iframe:not(.reframe-off), .page__content iframe:not(.reframe-off), .project-content iframe:not(.reframe-off)");
-
+  reframe(
+    ".post__content iframe:not(.reframe-off), .page__content iframe:not(.reframe-off), .project-content iframe:not(.reframe-off)"
+  );
 
   /* =======================
   // LazyLoad Images
   ======================= */
   var lazyLoadInstance = new LazyLoad({
-    elements_selector: ".lazy"
-  })
-
+    elements_selector: ".lazy",
+  });
 
   /* =======================
   // Zoom Image
   ======================= */
-  const lightense = document.querySelector(".page__content img, .post__content img, .project-content img, .gallery__image img"),
-  imageLink = document.querySelectorAll(".page__content a img, .post__content a img, .project-content a img, .gallery__image a img");
+  const lightense = document.querySelector(
+      ".page__content img, .post__content img, .project-content img, .gallery__image img"
+    ),
+    imageLink = document.querySelectorAll(
+      ".page__content a img, .post__content a img, .project-content a img, .gallery__image a img"
+    );
 
   if (imageLink) {
-    for (var i = 0; i < imageLink.length; i++) imageLink[i].parentNode.classList.add("image-link");
-    for (var i = 0; i < imageLink.length; i++) imageLink[i].classList.add("no-lightense");
+    for (var i = 0; i < imageLink.length; i++)
+      imageLink[i].parentNode.classList.add("image-link");
+    for (var i = 0; i < imageLink.length; i++)
+      imageLink[i].classList.add("no-lightense");
   }
 
   if (lightense) {
-    Lightense(".page__content img:not(.no-lightense), .post__content img:not(.no-lightense), .project-content img:not(.no-lightense), .gallery__image img:not(.no-lightense)", {
-    padding: 60,
-    offset: 30
-    });
+    Lightense(
+      ".page__content img:not(.no-lightense), .post__content img:not(.no-lightense), .project-content img:not(.no-lightense), .gallery__image img:not(.no-lightense)",
+      {
+        padding: 60,
+        offset: 30,
+      }
+    );
   }
-
 
   /* ============================
   // Testimonials Slider
@@ -85,25 +133,23 @@ document.addEventListener("DOMContentLoaded", function() {
         },
         0: {
           items: 1,
-        }
-      }
+        },
+      },
     });
   }
-
 
   /* =================================
   // Smooth scroll to the tags page
   ================================= */
-  document.querySelectorAll(".tag__link, .top__link").forEach(anchor => {
+  document.querySelectorAll(".tag__link, .top__link").forEach((anchor) => {
     anchor.addEventListener("click", function (e) {
       e.preventDefault();
 
       document.querySelector(this.getAttribute("href")).scrollIntoView({
-        behavior: "smooth"
+        behavior: "smooth",
       });
     });
   });
-
 
   /* =======================
   // Scroll Top Button
@@ -113,9 +159,8 @@ document.addEventListener("DOMContentLoaded", function() {
       window.scrollTo({
         top: 0,
         left: 0,
-        behavior: "smooth"
-      })
+        behavior: "smooth",
+      });
     }
   });
-
 });
